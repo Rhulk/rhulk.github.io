ec5e08456384b68a429757ce48137fd54e0792c7
kike <quique1904@gmail.com>
2020-07-30 12:57:23 +0200

    Añadido pestañas al formulario y Menu

---
+++ angular.nin.js
@@ -0,0 +1,12 @@
+$scope.Save function(){
+	$scope.formVisibility=true;
+	console.log($scope.formVisibility)
+}
+
+$scope.formVisibility false;
+
+$scope.ShowForm=function(){
+	$scope.formVisibility=true;
+	console.log($scope.formVisibility)
+		
+}
